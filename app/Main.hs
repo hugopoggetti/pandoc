0438e6382fb1f09e8ca11845d57bfe182f1a2579
dorian melenotte <dorian.melenotte@epitech.eu>
2025-04-22 17:20:02 +0200

    [ADD] lib for parsing

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,22 @@
 module Main (main) where
 
-import Lib
+import Lib (Parser, runParser,
+        parseChar,
+        parseUInt,
+        parseInt,
+        parseTuple,
+        parseAnyChar,
+        parseOr,
+        parseAnd,
+        parseAndWith,
+        parseMany,
+        parseSome)
+import Control.Applicative ((<|>), (<*>))
 
 main :: IO ()
-main = someFunc
+main = do
+    input <- getLine
+    let parser = parseTuple parseInt
+    case runParser parser input of
+        Just (result, rest) -> putStrLn $ "Parsed character: " ++ show result ++ ", remaining string: " ++ rest
+        Nothing -> putStrLn "Failed to parse input"
