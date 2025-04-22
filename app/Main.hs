3ba76153d0ac1d9513bcc7af948fbb67e1df192c
GuillaumeBlondel <guillaume1.blondel@epitech.eu>
2025-04-22 20:13:54 +0200

    Add -> opt parse system for parse options program

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,24 @@
+{-
+-- EPITECH PROJECT, 2025
+-- B-FUN-400-TLS-4-1-mypandoc-hugo.poggetti
+-- File description:
+-- Main
+-}
+
 module Main (main) where
 
-import Lib
+import OptsParserSystem
+
+import System.Environment (getArgs)
+import System.Exit
+
+start :: [String] -> Opts -> IO ()
+start args opts
+    | globalOptsChecker args opts == False =
+        putStrLn usage >> exitWith(ExitFailure 84)
+    | otherwise = return ()
 
 main :: IO ()
-main = someFunc
+main = do
+    args <- getArgs
+    start args (optsParser args)
