d9c682031c2f86ae149fbc6da12ef964059c89c9
naarie <naarie@core>
2025-04-23 20:10:01 +0200

    [ADD] parsing begining

--- app/Main.hs
+++ app/Main.hs
@@ -4,6 +4,7 @@
 -- File description:
 -- Main
 -}
+{-# OPTIONS_GHC -Wno-unused-imports #-}
 
 module Main (main) where
 
@@ -18,17 +19,64 @@ import Lib (Parser, runParser,
         parseAndWith,
         parseMany,
         parseSome)
+
 import Control.Applicative ((<|>), (<*>))
 import OptsParserSystem
-
 import System.Environment (getArgs)
 import System.Exit
+import System.IO
+import Text.Read (get)
+import Data.Maybe (fromJust)
+--import Debug.Trace
+
+splitOn :: Eq a => a -> [a] -> [[a]]
+splitOn _ [] = []
+splitOn delimiter s = case break (== delimiter) s of
+    (h, []) -> [h]
+    (h, t)  -> h : splitOn delimiter (drop 1 t)
+
+getFileExtension :: String -> String
+getFileExtension fname = last (splitOn '.' fname)
+
+getFileBaseName :: String -> String
+getFileBaseName fname = head (splitOn '.' fname)
+
+checkoptionnal :: Opts -> Opts
+checkoptionnal opts
+  | inputFormat opts == Nothing =
+      let ext = getFileExtension (fromJust(inputFile opts))
+          fmt = if ext == "md" then "markdown" else ext
+      in checkoptionnal opts { inputFormat = Just fmt }
+  | outputFile opts == Nothing =
+      let name = getFileBaseName (fromJust(inputFile opts))
+          out  = "./" ++ name ++ "." ++ fromJust(outputFormat opts)
+      in checkoptionnal opts { outputFile = Just out }
+  | otherwise = opts
+
+
+-- parsefile
+ 
 
 start :: [String] -> Opts -> IO ()
 start args opts
-    | globalOptsChecker args opts == False =
-        putStrLn usage >> exitWith(ExitFailure 84)
-    | otherwise = return ()
+    | not (globalOptsChecker args opts) =
+        putStrLn usage >> exitWith (ExitFailure 84)
+    | otherwise = return ()--parsefile
+        -- do 
+        -- putStrLn ((optsToList opts)!!0)
+        -- putStrLn ((optsToList opts)!!1)
+        -- putStrLn ((optsToList opts)!!2)
+        -- putStrLn ((optsToList opts)!!3)
+        -- putStrLn ((optsToList opts)!!4)
+        -- putStrLn ((optsToList opts)!!5)
+        -- putStrLn ((optsToList opts)!!6)
+        -- putStrLn ((optsToList opts)!!7)
+
+
+readthefile :: String -> IO String
+readthefile path = do
+    fileHandle <- openFile path ReadMode
+    hGetContents fileHandle
 
 main :: IO ()
 main = do
