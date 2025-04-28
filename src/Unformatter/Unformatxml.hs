5e87ae61c08c3e9daaf5d39d173b8de390561c32
naarie <naarie@core>
2025-04-28 14:20:22 +0200

    [ADD] Unfunctionnal parsing

--- src/Unformatter/Unformatxml.hs
+++ src/Unformatter/Unformatxml.hs
@@ -5,37 +5,70 @@
 -- Main
 -}
 {-# OPTIONS_GHC -Wno-unused-top-binds #-}
+{-# OPTIONS_GHC -Wno-missing-fields #-}
 
 module Unformatter.Unformatxml (parsexml) where
 
-import Ast.Document (Document)
+import Ast.Document (Document(..), Meta (..), newdoc, Inline (..))
+import Lib (Parser(runParser), parseString)
 import Utils
-import Debug.Trace
 
 data Xmlcutter = Xmlcutter {
-    tagWrap       :: String -> String -> String,
-   -- attribute     :: (String, String) -> String,
-    tagWithAttrs  :: String -> [String] -> String -> String,
-    --tagList       :: String -> [String] -> String,
-    metaXml       :: [(String, String)] -> String,
-    headerXml     :: Int -> String -> String,
-    textXml       :: String -> String -> String, -- e.g., tag-based formatting like <b>, <i>
-    linkXml       :: (String, String) -> String,
-    imageXml      :: (String, String) -> String
+   header :: [String],
+   body :: [String]
 }
 
 xmlCutterDefault :: Xmlcutter
 xmlCutterDefault = Xmlcutter {
-    tagWrap = \tag content -> "<" ++ tag ++ ">" ++ content ++ "</" ++ tag ++ ">",
-    --attribute = \(k, v) -> k ++ "=\"" ++ v ++ "\"",
-    tagWithAttrs = \tag attrs content -> "<" ++ tag ++ " " ++ unwords attrs ++ ">" ++ content ++ "</" ++ tag ++ ">",
-    --tagList = \tag contents -> concatMap (tagWrap tag) contents,
-    metaXml = \pairs -> "<meta>" ++ concatMap (\(k,v) -> "<" ++ k ++ ">" ++ v ++ "</" ++ k ++ ">") pairs ++ "</meta>",
-    headerXml = \n txt -> "<h" ++ show n ++ ">" ++ txt ++ "</h" ++ show n ++ ">",
-    textXml = \tag txt -> "<" ++ tag ++ ">" ++ txt ++ "</" ++ tag ++ ">",
-    linkXml = \(text, url) -> "<a href=\"" ++ url ++ "\">" ++ text ++ "</a>",
-    imageXml = \(alt, src) -> "<img alt=\"" ++ alt ++ "\" src=\"" ++ src ++ "\" />"
+   header = ["<header title=", "<author>", "<date>"],
+   body =  ["<paragraph>", "<section title=", "<bold>", "<italic>",
+   "<code>", "<codeblock>", "<list>", "<link url=", "<image url="]
 }
 
+getauthor :: (String, Document) -> (String, Document)
+getauthor (file, newfile) =
+   case runParser (parseString "<author>") file of
+    Just (result, rest) ->
+      let authParts = splitOn '<' rest
+          auth=if not (null authParts)then head authParts else "no auth"
+          newMeta = Meta {metaAuthors = [[Str auth]]}
+      in("<"++last authParts, Document newMeta (case newfile of Document _ blocks -> blocks))
+    Nothing -> (file, newfile)
+
+--getdate ::
+
+-- newMeta = Meta {
+--             metaTitle = [Str title],
+--             metaAuthors = [[Str author]],
+--             metaDate = [Str date]
+--         }
+--     in Document newMeta (case doc of Document _ blocks -> blocks)
+
+
+parseheader :: (String, Document) -> (String, Document)
+parseheader (file, newfile) =
+  case runParser (parseString "<header title=\"") file of
+    Just (result, rest) ->
+      let titleParts = splitOn '\"' rest
+          title=if not (null titleParts)then head titleParts++"\"" else "Untitl"
+          newMeta = Meta { metaTitle = [Str title]}
+      in(last titleParts, Document newMeta (case newfile of Document _ blocks -> blocks))
+    Nothing -> (file, newfile)
+
+getroot :: (String, Document) -> (String, Document)
+getroot (file, newfile) =
+   case runParser (parseString "<document/>\n") file of
+      Just (result, rest) -> (rest, newfile)
+      Nothing -> (file, newfile)
+
+parsingxml :: (String, Document) -> Document
+parsingxml (file, newfile) = newfile
+  where
+    (nfile, nnewfile) = getroot (file, newfile)
+
 parsexml :: String -> Document -> Document
-parsexml file = trace ("the file: " ++ show file)
+parsexml file newfile = parsingxml (file, newfile)
+    -- case runParser (parseChar '<') file of
+    --     Just (result, rest) -> trace ("Parsed character: " ++ show result ++ ", remaining string: " ++ rest)
+    --     Nothing -> trace "Failed to parse input"
+    --trace ("the file: " ++ show )--(getXmlTagContent "author" file))
