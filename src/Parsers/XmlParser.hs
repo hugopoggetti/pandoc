47fc812a0f5b8fc259ed99b49163132b311dc967
naarie <naarie@core>
2025-04-30 01:36:23 +0200

    [ADD] the block (untested yet)

--- src/Parsers/XmlParser.hs
+++ src/Parsers/XmlParser.hs
@@ -9,7 +9,7 @@
 
 module Parsers.XmlParser (parsexml) where
 
-import Ast.Document (Document(..), Meta (..), newdoc, Inline (..), Block)
+import Ast.Document (Document(..), Meta (..), newdoc, Inline (..), Block (..))
 import Lib (Parser(runParser), parseString)
 import Utils
 import Data.Maybe (fromJust)
@@ -24,6 +24,8 @@ xmlCutterDefault = Xmlcutter {
    body =  ["<paragraph>", "<section title=", "<bold>", "<italic>",
    "<code>", "<codeblock>", "<list>", "<link url=", "<image url="]
 }
+
+-- date and author meta
 
 parsedate :: String -> (String, Maybe String)
 parsedate file =
@@ -59,19 +61,23 @@ getDateAndAuth file =
             Nothing -> ("", Nothing, Nothing)
       Nothing -> ("", Nothing, Nothing)
 
+-- header parsing and title recuperation
+
 parseheader :: String -> (String, Maybe String, Maybe String, Maybe String)
 parseheader file =
   case runParser (parseString "<header title=\"") file of
     Just (_, rest) ->
       let titleParts = splitOne '\"' rest
           title= head titleParts
-      in  
+      in
          let (headerless, auth, date) = getDateAndAuth (last titleParts)
          in ((removeLeadingSpaces headerless), Just title, auth, date)
     Nothing -> ("", Nothing, Nothing, Nothing)
 
+-- matadata
+
 getheader :: (String, Document) -> (String, Maybe Document)
-getheader (file, newfile) = 
+getheader (file, newfile) =
    let (headerless, title, auth, date) = parseheader file
        newMeta = Meta {
          metaTitle = [Str (fromJust title)],
@@ -83,94 +89,210 @@ getheader (file, newfile) =
          (case newfile of Document _ blocks->blocks))
    in (headerless, ndoc)
 
+-- root file
+
 getroot :: (String, Document) -> (String, [String], Maybe Document)
 getroot (file, newfile) =
    case runParser (parseString "<document>\n") file of
       Just (result, rest) -> (removeLeadingSpaces rest, [result],Just newfile)
       Nothing -> ("", [], Nothing)
 
--- a
--- a
--- a
--- a
--- a
--- a
--- a
--- a
--- a
+-- bold inline
 
 getbold :: (String, [String], [Inline]) -> (String, [String], Maybe [Inline])
 getbold (file , array, inline) = (file , array, Just inline)
 
+isbold :: (String, Int, [String], [Inline]) -> (String, [String], Maybe [Inline])
+isbold (file, array, inline) =
+   case runParser (parseString "<bold>") file of
+    Just (_, rest) -> (rest, push "</paragraph>\n" array, Just inline)
+    Nothing -> islink (file , array, inline)
+
+-- italic inline
+
 getitalic :: (String, [String], [Inline]) -> (String, [String], Maybe [Inline])
 getitalic (file , array, inline) = (file , array, Just inline)
+
+isitalic :: (String, Int, [String], [Inline]) -> (String, [String], Maybe [Inline])
+isitalic (file, array, inline) =
+   case runParser (parseString "<italic>") file of
+    Just (_, rest) -> (rest, push "</paragraph>\n" array, Just inline)
+    Nothing -> isbold (file , array, inline)
+-- code text inline
 
 getcode :: (String, [String], [Inline]) -> (String, [String], Maybe [Inline])
 getcode (file , array, inline) = (file , array, Just inline)
 
+iscode :: (String, Int, [String], [Inline]) -> (String, [String], Maybe [Inline])
+iscode (file, array, inline) =
+   case runParser (parseString "<code>") file of
+    Just (_, rest) -> (rest, push "</paragraph>\n" array, Just inline)
+    Nothing -> isitalic (file , array, inline)
+
+-- THE SECOND STRING OF TARGET WILL BE EMPTY FOR THE MOMENT
+-- link inline
+
 getlink :: (String, [String], [Inline]) -> (String, [String], Maybe [Inline])
 getlink (file , array, inline) = (file , array, Just inline)
 
+islink :: (String, Int, [String], [Inline]) -> (String, [String], Maybe [Inline])
+islink (file, array, inline)= 
+   case runParser (parseString "<link url=\"") file of
+    Just (_, rest) -> (rest, push "</paragraph>\n" array, Just inline)
+    Nothing -> iscode (file , array, inline)
+
+-- image inline
+
 getimage :: (String, [String], [Inline]) -> (String, [String], Maybe [Inline])
-getimage (file , array, inline) = (file , array, Just inline)
+getimage (file , array, inline) = 
+   let newfile = splitOne "<" ++ file
+       (nfile, narray, ninlines) = isimage (newfile, array, [])
+   in 
 
--- a
--- a
--- a
--- a
+isimage :: (String [String], [Inline]) -> (String, [String], Maybe [Inline])
+isimage (file, array, inline)= 
+   case runParser (parseString "<image url=\"") file of
+    Just (_, rest) -> 
+      let content = splitOne '>' rest
+          ncontent = splitOne '<' (last content)
+          parray = push "</image>" array
+          (nfile,narray,image)=getimage(head ncontent,parray, [])
+      in (nfile, narray, Just(inline ++[Image(fromJust image)
+      (init(head content),"")]))
+    Nothing -> islink (file , array, inline)
 
-getcodeblock :: (String, [String], [Block]) -> (String, [String], Maybe [Block])
-getcodeblock (file , array, block) = (file , array, Just block)
+-- inlineless
 
-getlist :: (String, [String], [Block]) -> (String, [String], Maybe [Block])
-getlist (file , array, block) = (file , array, Just block)
+gettext :: (String, String, [String], String, [Inline]) ->
+   (String, [String], Maybe [Inline])
+gettext (file, content, array, "</bold>", inline) = getbold(file,array,inline)
+gettext (file, content, array,"</italic>",inline)=getitalic(file,array,inline)
+gettext (file, content, array, "</code>", inline) = getcode(file,array,inline)
+gettext (file, content, array, "</link>", inline) = getlink(file,array,inline)
+gettext (file, content, array, "</image>", inline)=getimage(file,array,inline)
+gettext (file, content, array, _, inline) = (file,array,Nothing)
 
-getparagraph :: (String, [String], [Block]) -> (String, [String], Maybe [Block])
-getparagraph (file , array, block) = (file , array, Just block)
+isless :: (String, [String], [Inline]) -> (String, [String], Maybe [Inline])
+isless (file, array, inline)= 
+   case runParser (parseString "<") file of
+    Just (_, _) -> isimage (file, array, inline)
+    Nothing -> 
+      let content = splitOne '<' file
+          (elem, _) = pop array
+      in if length array == 0 then (last content, array,
+      Just(inline ++ [Str(head content)])) else
+         gettext (last content, head content, array, fromJust elem, inline)
 
-getsection :: (String, [String], [Block]) -> (String, [String], Maybe [Block])
-getsection (file , array, block) = 
-   case runParser (parseString "<section title=\"") file of
-    Just (_, rest) ->
-      let sectionParts = splitOne '>' rest
-          section = head sectionParts
-      in (removeLeadingSpaces rest, push "</section>" array, Just block)
-    Nothing -> (file , array, Nothing)
+-- inlines
 
-isinline :: (String, [String], [Inline]) -> (String, [String], Maybe [Inline])
-isinline (file, array, inline)=let(bfil,barray,bin)=getbold(file,array,inline)
-   in if bfil /= file then (bfil, barray, bin) else
-   let (efile, earray, eblock) = getitalic (file, array, inline)
-   in if efile /= file then (efile, earray, eblock) else
-   let (cfile, carray, cblock) = getcode (file, array, inline)
-   in if cfile /= file then (cfile, carray, cblock) else
-   let (lfile, larray, lblock) = getlink (file, array, inline)
-   in if lfile /= file then (lfile, larray, lblock) else
-   let (ifile, iaray, iblock) = getimage (file, array, inline)
-   in if ifile/=file then(ifile,iaray, iblock)else(file,array,Just inline)
+isinline :: (String, Int, [String], [Inline]) -> 
+   (String, Int, [String], Maybe [Inline])
+isinline (file, level, array, inline)= 
+   let (nfile, narray, ninline) = isless (file, array, inline)
+   in if file == "" || ninline == Nothing then 
+   (file, level, array, Just inline) else
+   isinline (nfile, level, narray, fromJust ninline)
 
-isaclosing :: (String, [String], [Block]) -> (String, [String], Maybe [Block])
+-- closing balise
+
+isaclosing :: (String, [String], [a]) -> (String, [String], Maybe [a])
 isaclosing (file, array, block) =
   case runParser (parseString "</") file of
     Just (_, rest1) ->
       case array of
         (toclose:restArray) ->
-          case runParser (parseString toclose) rest1 of
+          case runParser (parseString toclose) file of
             Just (_, rest2)->(removeLeadingSpaces rest2,restArray,Just block)
             Nothing -> (file, array, Nothing)
         [] -> (file, array, Nothing)
-    Nothing -> (file, array, Nothing)
+    Nothing -> (file, array, Just block)
+
+-- paragraph
+
+getparagraph :: (String, [String], [Block]) -> (String, [String], Maybe [Block])
+getparagraph (file , array, block) =
+   let (content, newfile) = splitstr file "</paragraph>\n"
+       (_, _, array, inline) = if newfile == Nothing then
+        ("", 0,array, Nothing) else isinline (content, 0, [], [])
+   in if inline == Nothing then ("", array, Nothing) else
+      (removeLeadingSpaces (fromJust newfile), array,
+      Just(block ++ [Para (fromJust inline)]))
+
+isaparagraph :: (String,[String],[Block]) -> (String, [String], Maybe [Block])
+isaparagraph (file, array, block) =
+   case runParser (parseString "<paragraph>\n") file of
+    Just (_, rest) -> getparagraph (rest, array, block)
+    Nothing -> isasection (file , array, block)
+
+-- section
+
+getsection :: (String, [String], [Block]) -> (String, [String], Maybe [Block])
+getsection (file , array, block) =
+   let content = splitOne '>' file
+       ncontent = splitOne '\n' (last content)
+       level = getiteration array 0 0
+       newfile = removeLeadingSpaces (last ncontent)
+       (nfile, narray, nblock) = isablock (newfile, array, [])
+   in (nfile, narray,
+   Just (block ++ [Section level [Str (head content)] (fromJust nblock)]))
+
+isasection :: (String, [String], [Block]) -> (String, [String], Maybe [Block])
+isasection (file, array, block) =
+   case runParser (parseString "<section title=\"") file of
+    Just (_, rest) -> getsection (rest, push "</section>\n" array, block)
+    Nothing -> isacodeblock (file , array, block)
+
+-- codeblock
+
+getcodeblock :: (String, [String], [Block]) -> (String, [String], Maybe [Block])
+getcodeblock (file , array, block) =
+   let datatext = splitOne '<' file
+   in case runParser (parseString "</paragraph>\n") ("<"++last datatext) of
+      Just (_, r) ->
+         case runParser (parseString "</codeblock>\n") (removeLeadingSpaces r)of
+            Just(_, rest)->(removeLeadingSpaces rest,
+               array,Just (CodeBlock (head datatext) : block))
+            Nothing -> ("",array, Nothing)
+      Nothing -> ("",array, Nothing)
+
+isacodeblock :: (String, [String], [Block]) -> (String, [String], Maybe [Block])
+isacodeblock (file, array, block) =
+   case runParser (parseString "<codeblock>\n") file of
+    Just (_, rest) ->
+      case runParser (parseString "<paragraph>") (removeLeadingSpaces rest) of
+         Just (_, rest1) -> (rest, array, Just block)
+         Nothing -> isalist (file , array, block)
+    Nothing -> isalist (file , array, block)
+
+-- list
+
+getlist :: (String, [String], Int, [[Block]]) -> (String, [String], Maybe [[Block]])
+getlist (file , array, nb, block) =
+   let newfile = removeLeadingSpaces file
+       (nfile, narray, nblock) = isaparagraph (newfile, array, [])
+   in if getiteration narray 0 0<nb then(nfile,narray,
+   Just (block ++ [fromJust nblock])) else
+   getlist (nfile, narray, nb, block ++ [fromJust nblock])
+
+isalist :: (String, [String], [Block]) -> (String, [String], Maybe [Block])
+isalist (file, array, block) =
+   case runParser (parseString "<list>\n") file of
+    Just (_, rest) ->
+      let pfile = removeLeadingSpaces rest
+          parray = push "</list>\n" array
+          (nfile,narray,list)=getlist (pfile,parray,getiteration array 0 0,[])
+      in (nfile, narray, Just(block ++ [BulletList (fromJust list)]))
+    Nothing -> isaclosing (file , array, block)
+
+-- block finder
 
 isablock :: (String, [String], [Block]) -> (String, [String], Maybe [Block])
 isablock (file, array, block) =
-    let (pfile, parray, pblock) = getparagraph (file, array, block)
-    in if pfile /= file then (pfile, parray, pblock) else
-    let (sfile, sarray, sblock) = getsection (file, array, block)
-    in if sfile /= file then (sfile, sarray, sblock) else
-    let (cfile, carray, cblock) = getcodeblock (file, array, block)
-    in if cfile /= file then (cfile, carray, cblock) else
-    let (lfile, laray, lblock) = getlist (file, array, block)
-    in if lfile/=file then(lfile,laray, lblock)else(file,array,Just block)
+   let (nfile, narray, nblock) = isaparagraph (file, array, block)
+   in if file == "" || nblock == Nothing then (file, array, Just block) else
+   isablock (nfile, narray, fromJust nblock)
+
+-- body parsing
 
 parsebody :: (String, [String], Document) -> (String, [String], Maybe Document)
 parsebody (file, newfile, newdoc) = (file, newfile, Just newdoc)
@@ -182,13 +304,15 @@ getbodybalise (file, array, newfile) =
          Just newfile)
       Nothing -> ("", [], Nothing)
 
+-- xml file parsing
+
 parsingxml :: (String, Document) -> Maybe Document
 parsingxml (file, newfile) =
     let (nfile, array, ndoc) = getroot (file, newfile)
         (headless, head) = if ndoc == Nothing then (file, Nothing) else
          getheader (nfile, fromJust ndoc)
         (bodyfile, newarray, bodydoc) = if head == Nothing then ("", [], Nothing)
-         else getbodybalise(headless, array, fromJust head)
+         else getbodybalise (headless, array, fromJust head)
     in Just newfile
 
 parsexml :: String -> Document -> Maybe Document
--- src/Utils.hs
+++ src/Utils.hs
@@ -4,8 +4,13 @@
 -- File description:
 -- Main
 -}
+{-# OPTIONS_GHC -Wno-unrecognised-pragmas #-}
+{-# HLINT ignore "Use isNothing" #-}
 
-module Utils (splitOne, splitOn, joinWithComma, removeLeadingSpaces, push, pop) where
+module Utils (splitOne, splitOn, joinWithComma, removeLeadingSpaces, push, pop, getiteration, splitstr) where
+import Ast.Document (Inline)
+import Data.List
+import Data.Maybe (fromJust)
 
 splitOn :: Eq a => a -> [a] -> [[a]]
 splitOn _ [] = []
@@ -33,3 +38,27 @@ push x stack = x : stack
 pop :: [a] -> (Maybe a, [a])
 pop []     = (Nothing, [])
 pop (x:xs) = (Just x, xs)
+
+safeIndex :: [String] -> Int -> Maybe String
+safeIndex [] _ = Nothing
+safeIndex (x:_) 0 = Just x
+safeIndex (_:xs) n = safeIndex xs (n-1)
+
+getiteration :: [String]-> Int -> Int -> Int
+getiteration array index val
+    | safeIndex array index == Nothing = val
+    | fromJust(safeIndex array index) == "</section>" =
+        getiteration array (index+1) val + 1
+    | otherwise = getiteration array (index+1) val
+
+splitstr :: String -> String -> (String, Maybe String)
+splitstr delim str = case breakOn delim str of
+    (before, "")    -> (before, Nothing)
+    (before, rest)  -> (before, Just(drop (length delim) rest))
+
+breakOn :: String -> String -> (String, String)
+breakOn _ "" = ("", "")
+breakOn delim s@(x:xs)
+    | delim `isPrefixOf` s = ("", s)
+    | otherwise = let (before, after) = breakOn delim xs
+                  in (x : before, after)
